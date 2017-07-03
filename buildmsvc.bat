c557188e8ed25e8d3d6dfebe9bf272075eacc9e3
Nick Cano <nick@cano.com>
2017-07-03 02:34:38 -0700

    generate build folder if it doesnt exist

--- buildmsvc.bat
+++ buildmsvc.bat
@@ -2,6 +2,8 @@
 	echo Must be run from a "Visual Studio Command Prompt"
 ) else (
 
+if not exist "build" mkdir build
+
 if exist deps\\luajit\\src\\lua51.lib (
     echo NOT BUILDING LuaJIT BECAUSE lua51.lib ALREADY EXISTS.
     echo To force rebuild of LuaJIT, DELETE "deps\\luajit\\src\\lua51.lib"
@@ -10,4 +12,5 @@ if exist deps\\luajit\\src\\lua51.lib (
 	echo BUILDING LuaJIT BECAUSE lua51.lib DOESN'T EXISTS.
     cd deps\\luajit\\src && msvcbuild.bat && cd ..\\..\\..\\build && cmake -G %1 Win32 ..\\ && cd ..
 )
+
 )
